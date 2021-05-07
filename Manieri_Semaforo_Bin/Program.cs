a63d825b3e6df83c4f22d8cb94bfb06f9ca8dda3
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2021-05-07 09:02:39 +0200

    Add project files.

---
+++ Manieri_Semaforo_Bin/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Manieri_Semaforo_Bin
+{
+    class Program
+    {
+        private static int n;
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                n = 0;
+                Thread t1 = new Thread(() => Incrementa());
+                Thread t2 = new Thread(() => Decrementa());
+                t1.Start();
+                t2.Start();
+                while (t1.IsAlive) { }
+                while (t2.IsAlive) { }
+                Console.WriteLine(n);
+                Console.ReadLine();
+            }
+        }       
+        private static void Incrementa()
+        {
+            for(int i=0; i <= 1000000; i++)
+            {
+                n++;
+            }
+        }
+        private static void Decrementa()
+        {
+            for (int i = 0; i <= 1000000; i++)
+            {
+                n--;
+            }
+        }
+    }
+}
